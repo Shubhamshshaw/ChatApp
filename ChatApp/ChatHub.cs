0d49a1cddfd6cda7448c6a23a45c7c6c7495e367
shubham.shaw <shubham.shaw@keka.com>
2025-02-24 10:53:53 +0530

    userName send to user

--- ChatApp/ChatHub.cs
+++ ChatApp/ChatHub.cs
@@ -6,17 +6,19 @@ public class ChatHub : Hub
     static List<User> users = new List<User>();
     public async Task SendMessage(string senderId, string receiverId, string message)
     {
-        var receiver = users.Where(s=>s.UserName == receiverId).FirstOrDefault().ConnectionId;
-        await Clients.Client(receiver).SendAsync("ReceiveMessage", senderId, message);
+        var receiver = users.Where(s=>s.UserName == receiverId).Select(s=>s.ConnectionId);
+        await Clients.Clients(receiver).SendAsync("ReceiveMessage", senderId, message);
     }
 
     public override async Task OnConnectedAsync()
     {
+        var userName = $"User {users.Count + 1}";
+        await Clients.Caller.SendAsync("UserName", userName);
         // Track online users
         await base.OnConnectedAsync();
         var user = Context.User;
         Console.WriteLine("new connection, total: "+ ++counter);
-        users.Add(new User() { ConnectionId = Context.ConnectionId, UserId = Context.UserIdentifier, UserName = $"User {users.Count+1}"});
+        users.Add(new User() { ConnectionId = Context.ConnectionId, UserId = Context.UserIdentifier, UserName = userName});
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
