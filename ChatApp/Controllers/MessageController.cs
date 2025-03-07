aeb9fdbd3d558622ecc69e7aad2d4099eef2fd4c
shubham.shaw <shubham.shaw@keka.com>
2025-03-07 20:51:58 +0530

    Refactor GetInboxMessages to return ChatboxMessages
    Updated GetInboxMessages to return a ChatboxMessages object,
    providing additional chat details such as ChatId, ChatName,
    ActiveStatus, and ProfileURL. Added GetActiveStatus method
    to check user active status asynchronously. Introduced
    ChatboxMessages class to encapsulate chat details.

--- ChatApp/Controllers/MessageController.cs
+++ ChatApp/Controllers/MessageController.cs
@@ -28,15 +28,39 @@ namespace ChatApp.Controllers
         }
 
         [HttpGet("dm/{userId}/{receiverId}")]
-        public List<Message> GetInboxMessages(string userId, string receiverId)
+        public ChatboxMessages GetInboxMessages(string userId, string receiverId)
         {
-            List<Message> messages1 = ChatHub.messages;
             List<Message> messages = ChatHub.messages
                 .Where(m => (m.SenderId == userId && m.ReceiverId == receiverId) ||
                              (m.SenderId == receiverId && m.ReceiverId == userId))
                 .OrderBy(m => m.SentOn) // Order by SentOn to get the latest messages last
                 .ToList();
-            return messages;
+
+            var receiver = ChatHub.users.FirstOrDefault(u => u.UserId == receiverId);
+            ChatboxMessages chatboxMessages = new ChatboxMessages()
+            {
+                ChatId = receiverId,
+                ChatName = receiver?.UserName ?? "Unknown User",
+                ActiveStatus = GetActiveStatus(receiverId, new ChatHub()).Result,
+                ProfileURL = receiver?.ProfileUrl ?? "Unknown Profile URL",
+                ChatMessages = messages
+            };
+
+            return chatboxMessages;
+        }
+
+        private static async Task<ActiveStatus> GetActiveStatus(string userId, ChatHub chatHub)
+        {
+            // Implement your logic to get the active status of the user
+            var result = await chatHub.CheckActiveStatus(userId);
+            if (result)
+            {
+                return ActiveStatus.Available;
+            }
+            else
+            {
+                return ActiveStatus.Offline;
+            }
         }
     }
 }
---
+++ ChatApp/Models/ResponseObjects/ChatboxMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatApp.Models.ResponseObjects
+{
+    public class ChatboxMessages
+    {
+        public string ChatId { get; set; }
+        public string ChatName { get; set; }
+        public ActiveStatus ActiveStatus { get; set; }
+        public string ProfileURL { get; set; }
+        public List<Message> ChatMessages { get; set; }
+    }
+}
